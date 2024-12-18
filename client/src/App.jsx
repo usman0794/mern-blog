9af5e547577ec309487e344297e10476cd84929c
Muhammad Usman Ali <usmanjami794@gmail.com>
2024-12-18 20:56:13 +0500

    Footer Component Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Dashboard from "./pages/Dashboard";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
@@ -21,6 +22,7 @@ export default function App() {
         <Route path="/signIn" element={<SignIn />} />
         <Route path="/signUp" element={<SignUp />} />
       </Routes>
+      <Footer/>
     </BrowserRouter>
   );
 }
