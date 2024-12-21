b6c3fca540849a9daa761d03d88416a08ce5ac42
Muhammad Usman Ali <usmanjami794@gmail.com>
2024-12-21 06:40:47 +0500

    sidebar of Dashboard

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Sidebar } from "flowbite-react";
+import { HiUser, HiArrowSmRight } from "react-icons/hi";
+import { Link } from "react-router-dom";
+
+export default function DashSidebar({ tab, setTab }) {
+  return (
+    <Sidebar className="w-full md:w-56">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Link to="/dashboard?tab=profile">
+            <Sidebar.Item
+              active={tab === "profile"}
+              icon={HiUser}
+              label={"User"}
+              labelColor="dark"
+              onClick={() => setTab("profile")} // Update tab state on click
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
+          <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+}
