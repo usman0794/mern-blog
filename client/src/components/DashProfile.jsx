42ca92a60bc72c4bf3bf0ca947bfe1c4fcc673b4
Muhammad Usman Ali <usmanjami794@gmail.com>
2024-12-29 20:41:34 +0500

    Complete Create a post UI

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -13,9 +13,10 @@ import {
   signoutSuccess,
 } from "../redux/user/userSlice";
 import { HiOutlineExclamationCircle } from "react-icons/hi";
+import { Link } from "react-router-dom";
 
 export default function DashProfile() {
-  const { currentUser, error } = useSelector((state) => state.user);
+  const { currentUser, error, loading } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const [uploadStatus, setUploadStatus] = useState(""); // To track upload status
@@ -214,9 +215,20 @@ export default function DashProfile() {
           value={formData.password}
           onChange={handleChange}
         />
-        <Button type="submit" gradientDuoTone="purpleToBlue" outline>
-          Update
+        <Button type="submit" gradientDuoTone="purpleToBlue" outline disabled={loading}>
+          {loading ? "Updating..." : "Update"}
         </Button>
+        {currentUser.isAdmin && (
+          <Link to="/create-post">
+            <Button
+              type="Button"
+              gradientDuoTone="redToYellow"
+              className="w-full"
+            >
+              Create a post
+            </Button>
+          </Link>
+        )}
       </form>
       {uploadStatus && (
         <p className="mt-3 text-center text-red-500">{uploadStatus}</p>
