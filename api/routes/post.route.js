a66aa4261b2e82e5c1969a3616145469b61599c6
Muhammad Usman Ali <usmanjami794@gmail.com>
2025-01-01 16:48:22 +0500

    get Post API route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { create } from "../controllers/post.controller.js";
+import { create, getposts } from "../controllers/post.controller.js";
 import { verifyToken } from "../utils/verifyUser.js"; 
 import { uploadSingle } from "../middlewares/upload.js";
 
 const router = express.Router();
 
 router.post("/posts", verifyToken, uploadSingle, create);
+route.post("getposts", getposts)
 
 export default router;
